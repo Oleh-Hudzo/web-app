97a76b171b279696f25705d8da7e9df8b70e7070
Oleh-Hudzo <myhudzio@gmail.com>
2023-02-28 00:13:03 +0200

    test fix

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,1 +1,12 @@
-js
+// Import the necessary modules
+const request = require('supertest');
+const app = require('./server');
+
+// Define the test suite
+describe('Server', () => {
+  // Define the test case
+  it('should respond with 200 OK', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+});
